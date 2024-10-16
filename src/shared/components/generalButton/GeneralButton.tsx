9569ded222ad537ad440e7a33a69729042c5c092
mirkoEscobedo <escobedomirko@gmail.com>
2024-10-16 18:55:32 +0200

    added onClick prop to general button

--- src/shared/components/generalButton/GeneralButton.tsx
+++ src/shared/components/generalButton/GeneralButton.tsx
@@ -1,14 +1,22 @@
 import { RainbowButton } from '@/components/ui/rainbow-button';
-type GeneralButtonProps = { children: React.ReactNode; className?: string }; //sto dichiarando il tipo
+type GeneralButtonProps = {
+  children: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+}; //sto dichiarando il tipo
 
 const GeneralButton: React.FC<GeneralButtonProps> = ({
   children,
   className,
+  onClick,
 }) => {
   //destrutturazione
   return (
     <>
-      <RainbowButton className={`flex justify content px-3 ${className ?? ''}`}>
+      <RainbowButton
+        onClick={onClick}
+        className={`flex justify content px-3 ${className ?? ''}`}
+      >
         {children}
       </RainbowButton>
     </>
