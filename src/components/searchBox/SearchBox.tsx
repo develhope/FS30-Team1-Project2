8003cb78596e308c67d60aff9f700e81945fd7cd
mirkoEscobedo <escobedomirko@gmail.com>
2024-10-16 19:27:16 +0200

    fixed responsiveness and margins and padding of searchbox

--- src/components/searchBox/SearchBox.tsx
+++ src/components/searchBox/SearchBox.tsx
@@ -7,20 +7,22 @@ const SearchBox: React.FC = () => {
   const [search, setSearch] = useState('');
   return (
     <>
-      <div className="flex justify-center mt-20 flex-col items-center">
+      <div className="flex justify-center mt-20 w-full max-w-full md:max-w-xl lg:max-w-2xl mx-auto px-4">
         <ShineBorder
-          className="flex p-0 w-2/3"
+          className="flex p-0 w-full"
           color={['#A07CFE', '#FE8FB5', '#FFBE7B']}
           borderWidth={2}
         >
           <input
-            className="z-10"
+            className="z-10 flex-1 px-4 py-2 text-lg outline-none bg-transparent"
             type="text"
-            placeholder="Enter Link 🔗"
+            placeholder="Enter a thread/playlist/video Link 🔗"
             value={search}
             onChange={(event) => setSearch(event.target.value)}
           />
-          <GeneralButton>Submit</GeneralButton>
+          <GeneralButton className="text-lg px-4 py-2 mr-2">
+            Submit
+          </GeneralButton>
         </ShineBorder>
       </div>
     </>
