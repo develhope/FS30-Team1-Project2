3676672cf033107a09a29894b7ba03168f0a06c4
mirkoEscobedo <escobedomirko@gmail.com>
2024-10-21 21:56:17 +0200

    added auto focus on searchbar

--- src/components/searchBox/SearchBox.tsx
+++ src/components/searchBox/SearchBox.tsx
@@ -1,9 +1,13 @@
 import ShineBorder from '@/components/ui/shine-border';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import GeneralButton from '@/shared/components/generalButton/GeneralButton';
 
 const SearchBox: React.FC = () => {
   const [search, setSearch] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
   return (
     <>
       <div className="flex justify-center mt-20 w-full max-w-full md:max-w-xl lg:max-w-2xl mx-auto px-4">
@@ -13,6 +17,7 @@ const SearchBox: React.FC = () => {
           borderWidth={2}
         >
           <input
+            ref={inputRef}
             className="z-10 flex-1 px-4 py-2 text-lg outline-none bg-transparent"
             type="text"
             placeholder="Enter a thread/playlist/video Link 🔗"
