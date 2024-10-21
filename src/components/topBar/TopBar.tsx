7d48c127ac3062c7e0a0d25dbeb2acf17c1d2a73
mirkoEscobedo <escobedomirko@gmail.com>
2024-10-21 16:54:36 +0200

    added first topbar impl

---
+++ src/components/topBar/TopBar.tsx
@@ -0,0 +1,16 @@
+import TransformingButton from '@/shared/components/transforming_button/TransformingButton';
+import LoginForm from '../login_form/LoginForm';
+
+const TopBar: React.FC = () => {
+  return (
+    <>
+      <div className="flex justify-end m-4">
+        <TransformingButton
+          card_children={<LoginForm/>}
+          button_children={'Login'}
+        ></TransformingButton>
+      </div>
+    </>
+  );
+};
+export default TopBar;
